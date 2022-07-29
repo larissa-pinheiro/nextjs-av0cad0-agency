be1e63750a5897fd57e1f00e1f2ce3a15b12bcd5
Larissa Pinheiro <rissapins@gmail.com>
2022-07-29 17:31:58 -0300

    Include another circle and edit alt from img

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -5,7 +5,8 @@ import Circle from "./Circle.jsx";
 function Intro() {
   return (
     <div className={styles.container}>
-      <Circle backgroundColor="#b0ff49" />
+      <Circle backgroundColor="#b0ff49" left="-45vh" top="-45vh" />
+      <Circle backgroundColor="#0ff49b" right="-40vh" />
       <div className={styles.card}>
         <h1 className={styles.title}>
           <span className={styles.brand}>AV0CAD0 </span>DIGITAL PRODUCT AGENCY
@@ -14,10 +15,15 @@ function Intro() {
           Create live segments and target the right people for messages based on
           their behaviors.
         </p>
-        <button className={styles.button}>DISCOVER</button>
+        <button className={styles.button}>discover</button>
       </div>
       <div className={styles.card}>
-        <Image src="/img/Avocado.png" layout="fill" objectFit="cover" alt="" />
+        <Image
+          src="/img/Avocado.png"
+          layout="fill"
+          objectFit="cover"
+          alt="A man sitting and smiling. Under him you can see the Avocado logo"
+        />
       </div>
     </div>
   );
