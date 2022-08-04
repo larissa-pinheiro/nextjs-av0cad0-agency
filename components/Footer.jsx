2f390a8b1adb174267b817c586d90a3b7e2bfbe8
Larissa Pinheiro <rissapins@gmail.com>
2022-08-04 16:02:19 -0300

    create footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,39 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../styles/Footer.module.css";
+
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.cardL}>
+        <h1 className={styles.title}>Av0cad0 Creatives.</h1>
+        <h1 className={styles.linkTitle}>
+          <a href="/contact" passHref>
+            <span className={styles.linkText}>Work with us</span>
+            <Image src="/img/link.png" width="40px" height="40px" alt="" />
+          </a>
+        </h1>
+      </div>
+      <div className={styles.cardS}>
+        <div className={styles.cardItem}>
+          12 Adam Street NY <br /> USA
+        </div>
+        <div className={styles.cardItem}>
+          contact@example.com <br /> 123 1234 12332
+        </div>
+      </div>
+      <div className={styles.cardS}>
+        <div className={styles.cardItem}>
+          Follow Us: <br />
+          _FB _IN _BE _TW
+        </div>
+        <div className={styles.cardItem}>
+          @ 2022 BY MAY, <br />
+          All rights reserved
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
