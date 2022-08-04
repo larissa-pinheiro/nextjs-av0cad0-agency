9a70b6c6759ddee5f4590ff5b8e46808cec1058d
Larissa Pinheiro <rissapins@gmail.com>
2022-08-04 13:54:10 -0300

    insert data, services and testimonials into index

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Head from "next/head";
 import Intro from "../components/Intro";
+import Services from "../components/Services";
+import Testimonials from "../components/Testimonials";
+import { data } from "../data";
 
-export default function Home() {
+export default function Home({ services }) {
   return (
     <div>
       <Head>
@@ -9,6 +12,17 @@ export default function Home() {
         <meta name="description" content="Av0cad0 Digital Product Agency" />
       </Head>
       <Intro />
+      <Services services={services} />
+      <Testimonials />
     </div>
   );
 }
+
+// this method allows to fetch any data before rendering components or pages
+// after declaring it we have to put it as a prop in the function
+export const getStaticProps = async () => {
+  const services = data;
+  return {
+    props: { services },
+  };
+};
