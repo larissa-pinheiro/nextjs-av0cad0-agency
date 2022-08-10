6e0b443ffe9fafe73f56c2a7fdbb4fd0d153244d
Larissa Pinheiro <rissapins@gmail.com>
2022-08-10 13:13:14 -0300

    insert products page

---
+++ pages/products/[name].js
@@ -0,0 +1,56 @@
+import Image from "next/image";
+import { data } from "../../data";
+import Link from "next/link";
+import styles from "../../styles/Products.module.css";
+
+function Products({ product }) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.cardL}>
+        {product.images.map((img) => (
+          <div key={img.id} className={styles.imgContainer}>
+            <Image
+              src={img.url}
+              width="100%"
+              height="100%"
+              objectFit="cover"
+              layout="responsive"
+              alt=""
+            />
+          </div>
+        ))}
+      </div>
+      <div className={styles.cardS}>
+        <h1 className={styles.title}>{product.title}</h1>
+        <p className={styles.desc}>{product.longDesc}</p>
+        <button className={styles.button}>
+          <Link href="/contact">Contact</Link>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export const getStaticPaths = async () => {
+  const products = data;
+  const paths = products.map((item) => {
+    return {
+      params: { name: item.name },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (ctx) => {
+  const name = ctx.params.name;
+  const product = data.filter((item) => item.name === name)[0];
+  return {
+    props: { product },
+  };
+};
+
+export default Products;
